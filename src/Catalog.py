0a83f60a939ce12764a0387efb39eb997d58ce81
chung <chung.yang@engineer.uconn.edu>
2019-03-29 21:06:18 -0400

    change data structure that represents the inventory when it's loaded

--- src/Catalog.py
+++ src/Catalog.py
@@ -5,14 +5,13 @@ app = Flask("catalog")
 
 def _load_books(db_name):
     """
-    This function takes the database name and load the content into a list of tuples.
-    Database here is just a text file.
+    This function takes the database name and convert the content to a dictionary
 
     :param db_name: data base name
-    :return: column names, information about all books in the database
+    :return: dictionary with ID as key and a tuple that stores information of a book as value
     """
 
-    book_info = []
+    book_info = {}
 
     with open(db_name) as f:
         lines = f.readlines()
@@ -20,31 +19,41 @@ def _load_books(db_name):
         #First line is column names so we toss it
         for line in lines[1:]:
             col = re.split(",\s*", line.replace("\n",""))
-            book_info.append(tuple(col[i] for i in range(len(col))))
+            book_info[col[0]] = tuple(col[i] for i in range(1,len(col)))
 
     return book_info
 
 def _filter_by_topic(book_info, topic):
     """
-    Return books with specified topic
+    Return names and IDs of books with specified topic
 
     :param book_info: list of tuples that contain information of books
     :param topic: a string that specified the topic of interest
-    :return: dictionary and books' names and quantities
+    :return: dictionary that maps bookname to its id
     """
     sorted_books = {}
     topic = topic.lower()
 
-    for book in book_info:
-        book_topic = book[1].lower().replace(" ", "")
-        book_name = book[2]
-        book_quantity = int(book[3])
-
+    for book_id , info in book_info.items():
+        book_topic = info[0].lower().replace(" ", "")
+        book_name = info[1]
+        
         if book_topic == topic:
-            sorted_books[book_name] = book_quantity
+            sorted_books[book_name] = book_id
 
     return sorted_books
 
+# def _filter_by_item(book_info, item_id):
+#     """
+#      Return cost and number of items in stocks for a book with item_id
+#
+#     :param book_info: list of tuples that contain information of books
+#     :param item_id: unique id for the book
+#     :return: dictionary that stores the cost and number of items in stock for a book with item_id
+#     """
+#
+#     for book in book_info:
+#
 
 @app.route("/query/<topic>", methods=['GET'])
 @app.route("/query/<int:item_number>", methods=['GET'])
