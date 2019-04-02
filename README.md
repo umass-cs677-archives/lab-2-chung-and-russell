b2236e876c824bbb54ef347a0fcb7c84bc271ff6
chung <chung.yang@engineer.uconn.edu>
2019-04-02 00:12:04 -0400

    more descriptive test messagesand some README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 677 Lab 2
 
-This is the Git repo for 677 Lab 2. See http://lass.cs.umass.edu/~shenoy/courses/spring19/labs/lab2.html for a description of the lab. The lab is due on April 3, 23:55 hrs. Prior to submitting your project, replace this README file with the one that explains how to setup and run your code. Be sure to provide enough details fo us to run it in order to grade it.
+To run TestCatalog.py, make sure all servers are running and execute the following command: python3 TestCatalog.py
--- test/TestCatalog.py
+++ test/TestCatalog.py
@@ -5,62 +5,68 @@ class TestCatalog(unittest.TestCase):
 
     def test_query_by_topic(self):
 
-        response = urllib.request.urlopen("http://0.0.0.0:5000/query/graduate_school")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/query/graduate_school")
         data = json.load(response)
         dic = {"items": {"Xen and the Art of Surviving Graduate School": 3, "Cooking for the Impatient Graduate Student":4}}
         self.assertDictEqual(data,dic)
 
-        response = urllib.request.urlopen("http://0.0.0.0:5000/query/distributed_systems")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/query/distributed_systems")
         data = json.load(response)
         dic = {"items": {"How to get a good grade in 677 in 20 minutes a day": 1,
                          "RPCs for Dummies": 2}}
-        self.assertDictEqual(data, dic)
 
+        self.assertDictEqual(data, dic)
+        print("Query message by topic, passed")
 
     def test_query_by_item(self):
 
-        response = urllib.request.urlopen("http://0.0.0.0:5000/query/1")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/query/1")
         data = json.load(response)
         dict = list(data.values())[0]
         dict_keys = list(dict.keys())
 
         self.assertIn("COST", dict_keys)
         self.assertIn("QUANTITY", dict_keys)
+        print("Query message by item, passed")
 
         self.assertIsInstance(dict["COST"], float)
         self.assertIsInstance(dict["QUANTITY"], int)
+        print("Return types are correct")
 
 
     def test_update(self):
-        response = urllib.request.urlopen("http://0.0.0.0:5000/query/2")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/query/2")
         data = json.load(response)
         dict = list(data.values())[0]
         original_cost = dict["COST"]
         original_quantity = dict["QUANTITY"]
 
         # Make sure increase operation works correctly
-        response = urllib.request.urlopen("http://0.0.0.0:5000/update/2/cost/increase/5")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/update/2/cost/increase/5")
         data = json.load(response)
         dict = list(data.values())[0]
         updated_cost = dict["COST"]
 
         self.assertEqual(updated_cost - original_cost, 5)
+        print("Increase operation, passed")
 
         # Make sure decrease operation works correctly
-        response = urllib.request.urlopen("http://0.0.0.0:5000/update/2/quantity/decrease/3")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/update/2/quantity/decrease/3")
         data = json.load(response)
         dict = list(data.values())[0]
         updated_quantity = dict["QUANTITY"]
 
         self.assertEqual(updated_quantity - original_quantity, -3)
+        print("Decrease operation, passed")
 
         # Make sure set operation works correctly
-        response = urllib.request.urlopen("http://0.0.0.0:5000/update/2/quantity/set/120")
+        response = urllib.request.urlopen("http://128.119.243.164:5000/update/2/quantity/set/120")
         data = json.load(response)
         dict = list(data.values())[0]
         updated_quantity = dict["QUANTITY"]
 
         self.assertEqual(updated_quantity, 120)
+        print("Set operation, passed")
 
 
 
