8c8d77892a328ab3e65038c06b44dd5f4b55432a
russell-lee <mail.russelllee@gmail.com>
2019-04-03 17:24:22 -0400

    adding restock from order, concurrency test
    -added emergency restock option to order.py
    -added text file of server addresses for convenience
    -added concurrent client shell script

--- test/PerTierExperiments.sh
+++ test/PerTierExperiments.sh
@@ -5,7 +5,7 @@ wget -qO- http://128.119.243.164:5002/update/1/quantity/set/0
 curl http://128.119.243.147:5001/orders -X DELETE -v
 python ../src/Client.py buy 1 1000 tierClient
 wget -qO- http://128.119.243.147:5001/orders>orders.txt
-cat orders.txt | sed -n 1'p' | tr ',' '\n' | grep '^"p' | grep -Eo "[0-9]+\.[0-9]+" > buy_query_times.txt
+cat orders.txt | sed -n 1'p' | tr ',' '\n' | grep '^"p' | grep -Eo "[0-9]+\.[0-9]+" > experiments/buy_query_times.txt
 
 
 
